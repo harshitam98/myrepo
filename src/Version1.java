b02fc5f037ddad7a2c4c0c8714db824b0e42a0b1
Harshita Mishra <harshitanmh98@gmail.com>
2025-07-13 19:15:35 +0530

    2nd version with add()

--- src/Version1.java
+++ src/Version1.java
@@ -1,5 +1,9 @@
 public class Version1 {
+    public static void add(int a, int b){
+        System.out.println("The addition of " +a +" & "+ b +" is : "+(a+b));
+    }
     public static void main(String[] args) {
         System.out.println("Hi, Welcome to Version 1");
+        add(5,4);
     }
 }
