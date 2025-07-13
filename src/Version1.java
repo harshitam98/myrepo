0f5575a802c713de8b8467de09365cccb239e44d
Harshita Mishra <harshitanmh98@gmail.com>
2025-07-13 19:24:18 +0530

    3rd version using sub()

--- src/Version1.java
+++ src/Version1.java
@@ -2,8 +2,13 @@ public class Version1 {
     public static void add(int a, int b){
         System.out.println("The addition of " +a +" & "+ b +" is : "+(a+b));
     }
+
+    public static void sub(int a, int b){
+        System.out.println("The subtraction of " +a +" & "+ b +" is : "+(a-b));
+    }
     public static void main(String[] args) {
         System.out.println("Hi, Welcome to Version 1");
         add(5,4);
+        sub(5,4);
     }
 }
